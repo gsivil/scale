5d5747dde799c27f5b25f504fcceba5ba95ad5d9
Georgios Siviloglou <gsivil@gmail.com>
2016-09-17 19:07:52 +0200

    jaren

--- b2b.c
+++ b2b.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 #include <math.h>
 
@@ -7,6 +8,8 @@
 void initpcword(bool pcword[]);
 void printpcword(bool pcword[]);
 void invertbit(bool pcword[], int n);
+void inttobin(unsigned int decint, bool pcword[]);
+unsigned int bintoint(bool pcword[]);
 
 const bool down = 0;
 const bool up = 0;
@@ -14,12 +17,33 @@ int main(void)
 {
     bool w1[wordsize], w2[wordsize];
     initpcword(w1); 
-    initpcword(w2);
-    invertbit(w1, 4);
+    printpcword(w1);
+    printf("%d", bintoint(w1));
+    invertbit(w1, 7);
     putchar('\n');
     printpcword(w1);
-    printpcword(w2);
+    printf("%d", bintoint(w1));
+    putchar('\n');
     return 0;
+}
+unsigned int bintoint(bool pcword[])
+{
+    unsigned int decint = 0;
+    unsigned int x = 1;
+    x = (x<< 14);
+    for (int i=wordsize-1;i>0;i--)
+    {
+        x = x>>1;
+        decint = decint + x*pcword[i];
+        printf("%d %d\n", x, decint);
+        if (i == 0)
+            return decint;
+    }
+}
+void inttobin(unsigned int decint, bool pcword[])
+{   
+    initpcword(pcword);
+    int binpos;
 }
 void invertbit(bool pcword[], int n)
 {
