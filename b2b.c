b7a4a396fda8c68471bef8f545910f7ee47981d3
Georgios Siviloglou <gsivil@gmail.com>
2016-09-17 22:57:14 +0200

    bit to usigned int

--- b2b.c
+++ b2b.c
@@ -3,41 +3,43 @@
 #include <stdbool.h>
 #include <math.h>
 
-#define wordsize 8
-#define bytesize 8
+#define wordsize 8 
 
 void initpcword(bool pcword[]);
 void printpcword(bool pcword[]);
 void invertbit(bool pcword[], int n);
 void inttobin(unsigned int decint, bool pcword[]);
 unsigned int bintoint(bool pcword[]);
+void invertallbits(bool pcword[]);
 
 const bool down = 0;
-const bool up = 0;
+const bool up = 1;
 int main(void)
 {
     bool w1[wordsize], w2[wordsize];
     initpcword(w1); 
     printpcword(w1);
     printf("%d", bintoint(w1));
-    invertbit(w1, 0);
-    invertbit(w1, 2);
     putchar('\n');
+    invertallbits(w1);
     printpcword(w1);
     printf("%d", bintoint(w1));
     putchar('\n');
     return 0;
 }
+void invertallbits(bool pcword[])
+{
+    for (int i =0;i<wordsize;i++)
+        pcword[i] = ~pcword[i];
+}
 unsigned int bintoint(bool pcword[])
 {
     unsigned int decint = 0;
     unsigned int x = 1;
-    x = (x<< 14);
-    for (int i=wordsize-1;i>0;i--)
+    for (int i=0;i<wordsize;i=i+1)
     {
-        x = x>>1;
-        decint = decint + x*pcword[i];
-        printf("%d %d\n", x, decint);
+        decint = decint + x*pcword[wordsize-i-1];
+        x = x << 1;
     }
     return decint;
 }
@@ -57,8 +59,8 @@ void initpcword(bool pcword[])
 }
 void printpcword(bool pcword[])
 {
-    for (int n = 0;n<=wordsize;n++)
-        printf("%d", pcword[wordsize-n]);
+    for (int n = 0;n<wordsize;n++)
+        printf("%d", pcword[n]);
 
     putchar('\n');
 }
