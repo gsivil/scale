cdd5b975f298b6303ffced8eb1d5ea42765d3395
Georgios Siviloglou <gsivil@gmail.com>
2016-09-20 00:45:05 +0200

    fseek and tell

--- bif.c
+++ bif.c
@@ -10,9 +10,16 @@ int main(void)
     fp1 = stdin;
     fp2 = stdout;
     char nextchar = 'a';
+    long int pos  = 0;
     while (nextchar !='x')
     {
-        fprintf(fp2, "%c", '\b');
+        fprintf(fp2, "\b%c", '\b');
+        pos = ftell(fp3);
+        if (pos%3 == 0)
+        {
+            fseek(fp3, pos, SEEK_SET);
+            printf("%ld ", pos);
+        };
         fprintf(fp3,"%c", (nextchar = getc(fp1))-0);
     };
     fclose(fp3);
--- test.c
+++ test.c
@@ -1,2 +1,2 @@
-9
+aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 x
