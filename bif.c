bcbea7eb7537b608bcdc1749af7575c33ff0f1d5
Georgios Siviloglou <gsivil@gmail.com>
2016-09-29 22:24:56 +0200

    start again ;

--- bif.c
+++ bif.c
@@ -1,10 +1,25 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void clc(void);
 
 int main(void)
 {
     FILE* fp1;
     fp1 = stdout;
-    fprintf(fp1, "%s\n", "Hello, github!");
+    char* greeting = "Hello, git!";
+    clc();
+    for (int i = 0;i<strlen(greeting);i++)
+    {
+        fprintf(fp1, "%c", greeting[i]);
+    };
+    putchar('\n');
     fclose(fp1);
     return 0;
+}
+
+void clc(void)
+{
+    system("clear");
 }
