cd99e0d544351abbbd4e993f6e94da69df313d91
Georgios Siviloglou <gsivil@gmail.com>
2016-10-03 22:27:25 +0200

    next util

--- genu.c
+++ genu.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define COUNT 10
+#define COUNT 3 
 struct elem {int key; int data;} Table[COUNT];
 int key_compare(const void* e1, const void* e2);
 
 int main(void)
 {
-    int Table[COUNT] = {5, 6, 7, 8 , 9, 10, 11, 12, 13, 14};
+    Table = {0, 42, 432};
     struct elem* result;
     struct elem key_elem;
     key_elem.key = 11;
 
-    result = (struct elem *) bsearch(&key_elem, Table, COUNT, sizeof(struct elem), key_compare);
-//    printf("%d\n", result->data);
+    result = (struct elem *) bsearch(&key_elem, &Table[0], COUNT, sizeof(struct elem), key_compare);
+    printf("%d\n", result->data);
     return 0;
 }
 
