cd09863cc3a2c19b932257e840fb1d19517442ab
Georgios Siviloglou <gsivil@gmail.com>
2016-10-07 00:54:03 +0200

    diff

--- genu.c
+++ genu.c
@@ -7,10 +7,30 @@
 // Replaced letter
 // One less letter
 // N less letters
+// the same for words
+// punctuation
 
+#define text1 "1.txt"
+#define text2 "2.txt"
 
 int main(void)
 {
+    FILE* fp1;
+    FILE* fp2;
+    
+    fp1 = fopen(text1, "r");
+    fp2 = fopen(text2, "r");
+    char next1 = 'a';
+    char next2 = 'a';
+    while (((next1 = fgetc(fp1))  != EOF) && ((next2 = fgetc(fp2))  != EOF))
+    {
+        printf("%c", next1);
+        if (next1 != next2)
+            printf("*");
+        printf("%c", next2);
+    };
+    fclose(fp2);
+    fclose(fp1);
     return 0;
 }
 
