d270ba509005530139fde9abbdbce83367068a55
Georgios Siviloglou <gsivil@gmail.com>
2016-10-07 00:25:30 +0200

    write diff

--- genu.c
+++ genu.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// A script to find trivial differences between two files
+// One extra letter
+// N extra letters
+// Replaced letter
+// One less letter
+// N less letters
+
+
 int main(void)
 {
-    system("wget https://arxiv.org/robots.txt -O foo.html");
-    putchar('\n');
     return 0;
 }
 
