c4f9f9e437f7ebbe8a83ad2081ebfc80a32b1204
Georgios Siviloglou <gsivil@gmail.com>
2016-10-08 23:36:13 +0200

    diff char

--- difc.c
+++ difc.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // A script to find trivial differences between two files
 // One extra letter
@@ -14,19 +15,40 @@
 //
 //
 void diffword(char* word1, char* word2);
+int diffnumchar(char* word1, char* word2);
 
 int main(void)
 {
     
     char* word1 = "Georgios";
-    char* word2 = "Heorgios";
+    char* word2 = "georgioS";
     char word3[] = "Geoorgios";
     diffword(word1, word2);
+    diffnumchar(word1, word2);
+    printf("%c\n", *word1);
     return 0;
 }
 
 void diffword(char* word1, char* word2)
 {
+    int compare = strcmp(word1, word2);
+    printf("%s\n", word1);
+    printf("%s\n", word2);
+    printf("%d\n", compare);
+}
+
+int diffnumchar(char* word1, char* word2)
+{
+    int c1, c2;
+    c1 = 0;
+    c2 = 0;
+    char* w1;
+    w1 = word1;
+    char* w2;
+    w2 = word2;
+    word1++;
     printf("%c\n", *word1);
-    printf("%c\n", *word2);
+    printf("%c\n", *w1);
+
+    return c1-c2;
 }
