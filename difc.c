46ec995f7981dfa5001299524f5251ec5b49de63
Georgios Siviloglou <gsivil@gmail.com>
2016-10-08 23:46:33 +0200

    diff char

--- difc.c
+++ difc.c
@@ -24,8 +24,10 @@ int main(void)
     char* word2 = "georgioS";
     char word3[] = "Geoorgios";
     diffword(word1, word2);
-    diffnumchar(word1, word2);
+
     printf("%c\n", *word1);
+    putchar('\n');
+    printf("%d\n", diffnumchar(word1, word2));
     return 0;
 }
 
@@ -39,16 +41,20 @@ void diffword(char* word1, char* word2)
 
 int diffnumchar(char* word1, char* word2)
 {
-    int c1, c2;
-    c1 = 0;
-    c2 = 0;
-    char* w1;
-    w1 = word1;
-    char* w2;
-    w2 = word2;
-    word1++;
-    printf("%c\n", *word1);
-    printf("%c\n", *w1);
+    int c1 = 0;
+    int  c2 = 0;
+    char* w1 = word1;
+    char* w2 = word2;
+    while ( *word1 != '\0')
+    {
+        printf("%d.%c ", c1++, *word1++);
+    };
+    putchar('\n');
+    while ( *word2 != '\0')
+    {
+        printf("%d.%c ", c2++, *word2++);
+    };
+    putchar('\n');
 
     return c1-c2;
 }
