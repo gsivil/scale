955012428e058e9859e416286657bfab5c82255c
Georgios Siviloglou <gsivil@gmail.com>
2016-10-09 01:19:01 +0200

    compare dna

--- difNA.c
+++ difNA.c
@@ -3,11 +3,13 @@
 #include <string.h>
 
 // Compare DNA sequences TAGC
-
+// 1 2 3 4-letter words
 
 int main(void)
 {
     char* dna1 = "TAGC";
-    char* dna2 = "
+    char* dna2 = "TTGC";
+    dna1++;
+    printf("%c\n", *dna1);
     return 0;
 }
