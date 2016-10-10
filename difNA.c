0b0d7df353f199225d6d442d6a2dd2e7a2ae8bb1
Georgios Siviloglou <gsivil@gmail.com>
2016-10-10 23:40:37 +0200

    check seq

--- difNA.c
+++ difNA.c
@@ -5,22 +5,73 @@
 // Compare DNA sequences TAGC
 // 1 2 3 4-letter words
 
+
+#ifndef max
+    #define max(a,b) ((a) > (b) ? (a) : (b))
+#endif
+
+#ifndef min
+    #define min(a,b) ((a) > (b) ? (b) : (a))
+#endif
+
+#define same 's'
+#define dif  'd'
+
+char diffDNA(char* dna1, char* dna2);
+char chareq(char d1, char d2);
+
 int main(void)
 {
     char* dna1 = "TAGC";
-    char* dna2 = "TTGC";
-    char* d1;
-    d1 = dna1;
-    d1++;
-    d1++;
-    dna1++;
-    printf("%c\n", *d1);
+    char* dna2 = "TAGC";
+    char totcheck;
+    totcheck =  diffDNA(dna1, dna2);
+    printf("%c\n", totcheck); 
     return 0;
 }
-
-void diffDNA(char* dna1, char* dna2)
+char diffDNA(char* dna1, char* dna2)
 {
-    char* d1;
-    char* d2;
+    char* d1 = dna1;
+    char* d2 = dna2;
+    int c1 = strlen(d1);
+    int c2 = strlen(d2);
+    char samenot;
+    char totcheck = 'S';
+    for (int i = 0;i<min(c1, c2);i++)
+    {
+        samenot = chareq(*d1, *d2);
+        if (samenot == dif)
+            totcheck = 'D';
+        printf("%d %c %c %c\n", i, *d1++, *d2++, samenot);
+    };
 
+    char space = '_';
+    if (c1 > c2)
+        for (int i=c2;i<c1;i++)
+        {
+            samenot = chareq(*d1, *d2);
+            if (samenot == dif)
+                totcheck = 'D';
+            printf("%d %c %c %c\n", i,  *d1++, space, samenot );
+        }
+    else if (c2 > c1)
+        for (int i=c1;i<c2;i++)
+        {
+            samenot = chareq(*d1, *d2);
+            if (samenot == dif)
+                totcheck = 'D';
+            printf("%d %c %c %c\n", i, space, *d2++, samenot );
+        };
+
+    return totcheck;
+}
+
+char chareq(char d1, char d2)
+{
+    char sameornot;
+    if (d1 == d2)
+        sameornot = same;
+    else
+        sameornot = dif;
+    return sameornot;        
 }
