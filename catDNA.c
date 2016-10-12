5c3e3f948daefa3c40466cfc868e6ea27391bde2
Georgios Siviloglou <gsivil@gmail.com>
2016-10-12 22:13:43 +0200

    perm of TAGCs

--- catDNA.c
+++ catDNA.c
@@ -3,16 +3,47 @@
 #include <string.h>
 
 #define listDNA "listDNA.txt"
+#define size 4
+//TAGC
 
-
+char DNA[size] = {'T', 'A', 'G', 'C'};
 
 int main(void)
 {
+
     FILE* fp;
     fp = fopen(listDNA, "w");
-    char DNA[4];
-    DNA[0] = 'T';
     fputc(DNA[0], fp);
+    int n = 0;
+    for (int i=0;i<size;i++)
+    {
+        fprintf(fp, "%d %c\n", n, DNA[i]);
+        n = n+1;
+    };
+    for (int i=0;i<size;i++)
+        for (int j=0;j<size;j++)
+        {
+            fprintf(fp, "%d %c %c\n", n, DNA[i], DNA[j]);
+            n = n+1;
+        };
+    for (int i=0;i<size;i++)
+        for (int j=0;j<size;j++)
+            for (int k=0;k<size;k++)
+            {
+                 fprintf(fp, "%d %c %c %c\n", n, DNA[i], DNA[j], DNA[k]);
+                 n = n+1;
+            };
+
+    for (int i=0;i<size;i++)
+        for (int j=0;j<size;j++)
+            for (int k=0;k<size;k++)
+                for (int l=0;l<size;l++)
+                    {
+                    fprintf(fp, "%d %c %c %c %c\n", n, DNA[i], DNA[j], DNA[k], DNA[l]);
+                    n = n+1;
+                    };
     fclose(fp);
     return 0;
 }
+
+
