09c08f7384bf83dd95ef99c715b6c41572b4a8b5
Georgios Siviloglou <gsivil@gmail.com>
2016-10-15 19:21:39 +0200

    count words

--- replace.c
+++ replace.c
@@ -6,6 +6,7 @@ int main(void)
 {
     char text[] = "The British Broadcasting Corporation (BBC) is a British public service broadcaster. It is headquartered at Broadcasting House in London, and is the world's oldest national broadcasting organisation[3] and the largest broadcaster in the world by number of employees, with over 20,950 staff in total, of whom 16,672 are in public sector broadcasting;[2][4][5][6][7] including part-time, flexible as well as fixed contract staff, the total number is 35,402.[8] The BBC is established under a Royal Charter[9] and operates under its Agreement with the Secretary of State for Culture, Media and Sport.[10] Its work is funded principally by an annual television licence fee[11] which is charged to all British households, companies, and organisations using any type of equipment to receive or record live television broadcasts.[12] The fee is set by the British Government, agreed by Parliament,[13] and used to fund the BBC's extensive radio, TV, and online services covering the nations and regions of the UK. From 1 April 2014, it also funds the BBC World Service, launched in 1932, which provides comprehensive TV, radio, and online services in Arabic, and Persian, and broadcasts in 28 languages.";
     char* token;
+    char* string1 = "the";
     token  = strtok(text, " ");
 //    printf("%s\n", token);
     int i = 0;
@@ -14,12 +15,14 @@ int main(void)
         token = strtok(NULL, " ");
         if (token != NULL)
         {
-            if (token[strlen(token) - 1] == '.')
+            if (!strcmp(token, string1))
+            {
                 i = i+1;
-            if (!strcmp(token, "28"))
-                printf("%d %s \n", strlen(token), token);
+            };
         };
     };
+    printf("'%s' has %d characters and appears %d times\n",string1, strlen(string1),i );
+
     return 0;
 }
 
