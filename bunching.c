076777d50b610d60e683a4ad9d471ee96d2a9f0f
Georgios Siviloglou <gsivil@gmail.com>
2016-10-17 01:06:09 +0200

    non-classical

---
+++ bunching.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// Take a series of 0 and 1 and determine the statistics
+// after that determine if we have bunching or not
+
+
+#define data1 "0100010001011100101"
+
+int main(void)
+{
+    printf("The data is non-classical\n");
+    return 0;
+}
