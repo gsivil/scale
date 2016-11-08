b2e43b1e5749b3655c957476bbb29d3c8aed83fa
Georgios Siviloglou <gsivil@gmail.com>
2016-11-09 00:34:09 +0100

    some comments

--- neural.c
+++ neural.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
+//Input layer, hidden layer, output layer
+//2 inputs and 2 outputs
+// Sigmoid function 
+// f(s) = 1/(1+exp(-a*s)
+// the inputs give s and s is the argument in the whatever decision function
+// there are intermediate input/output layers to convey they information
+// training means to change the weights w_i in the funct(w_i*a_i)
+// if a training input output gives a wrong output
+// we modify the weights till it give a correct answer
+// then 
 int main(void)
 {
     printf("Train a neural netword");
